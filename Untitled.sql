d6b81f39505658fd76c876fb770f2f3059b2d09a
Srashta Maharjan <smaharjan@vassar.edu>
2020-11-14 19:00:34 -0500

    sql

---
+++ Untitled.sql
@@ -0,0 +1,7 @@
+USE mutual_schema;
+
+SELECT * FROM mutual_table;
+#SELECT name FROM mutual_table;
+#SELECT * FROM mutual_table WHERE needLevel LIKE '3'
+#SELECT * FROM mutual_table WHERE needLevel >3
+#SELECT name, email FROM mutual_table WHERE needLevel >3
